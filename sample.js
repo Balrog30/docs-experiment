e7edd4c0340988f8ac1ab1641d42821377613088
Nicholas Bering <nick@whitemeadowsfarms.com>
2015-08-31 05:26:50 +0000

    Add app and controller properties to page frontmatter. Remove Demo layout and simplify HTML for examples.

--- sample.js
+++ sample.js
@@ -1,9 +1,13 @@
 /* global angular */
 
 angular.module("google-chart-sample", ["googlechart"]).value('googleChartApiConfig', {
-            version: '1',
-            optionalSettings: {
-                packages: ['corechart', 'gauge'],
-                language: 'fr'
-            }
-    });
+    version: '1',
+    optionalSettings: {
+        packages: ['corechart', 'gauge'],
+        language: 'fr'
+    }
+})
+.config(['$interpolateProvider', function($interpolateProvider){
+    $interpolateProvider.startSymbol('!!');
+    $interpolateProvider.endSymbol('!!');
+}]);
